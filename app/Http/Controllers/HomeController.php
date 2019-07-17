c86af36aea89ff23b5fb772d4d3a110ededa5829
davidstar333 <david1213117@gmail.com>
2019-07-18 02:59:01 +0400

    server push

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -9,6 +9,7 @@ use Carbon\Carbon;
 
 use App\User;
 use App\Models\Transaction;
+use App\Models\Company;
 use App\Models\Category;
 use App\Models\Account;
 
@@ -31,10 +32,24 @@ class HomeController extends Controller
      */
     public function index(Request $request) {        
         config(['site.page' => 'home']);
-        $search_users = User::pluck('id')->toArray();
+        $company = Company::first();
+        $companies = Company::all();
+        $user = Auth::user();
         $search_categories = Category::pluck('id')->toArray();
-        $search_accounts = Account::pluck('id')->toArray();
-        $first_transaction = Transaction::orderBy('timestamp')->first();
+        
+        if($user->hasRole('user')){
+            $company = $user->company;
+        }else{            
+            if ($request->get('company_id') != ""){   
+                $selected_company = $request->get('company_id');
+                $company = Company::find($selected_company);
+            }   
+        }
+        $company_id = $company->id;
+        $search_users = $company->users()->pluck('id')->toArray();
+        $search_accounts = $company->accounts()->pluck('id')->toArray();
+        $first_transaction = $company->transactions()->orderBy('timestamp')->first();
+
         if($first_transaction){
             $from = date('Y-m-d', strtotime("-1 day", strtotime($first_transaction->timestamp)));
         }else{
@@ -43,7 +58,8 @@ class HomeController extends Controller
         
         // $from = Carbon::now()->startOfMonth()->format('Y-m-d');
         $to = date('Y-m-d', strtotime("+1 days"));
-        $period = $spec_date = '';        
+        $period = $spec_date = ''; 
+            
         if ($request->get('spec_date') != ""){   
             $spec_date = $request->get('spec_date');
             $to = $spec_date;
@@ -52,7 +68,7 @@ class HomeController extends Controller
             $from = substr($period, 0, 10);
             $to = substr($period, 14, 10);
         }
-        return view('home', compact('period', 'search_users', 'search_categories', 'search_accounts', 'from', 'to', 'spec_date'));
+        return view('home', compact('period', 'companies', 'company', 'search_users', 'search_categories', 'search_accounts', 'company_id', 'from', 'to', 'spec_date'));
     } 
     
     public function set_pagesize(Request $request){
